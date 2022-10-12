e55aec42567a136a9a6e23ed48cbb7c1ca97dc73
Baptiste <baptiste.seneclauze@gmail.com>
2022-10-12 11:31:25 +0200

    exercice matrice

--- TP_2/TP_2_question_2.c
+++ TP_2/TP_2_question_2.c
@@ -4,7 +4,8 @@
 #define SIZE 5
 typedef int matrice;
 
-void matrix_mult(matrice resultat[][SIZE], matrice matrice1[][SIZE], matrice matrice2[][SIZE]){
+
+void matrix_mult(matrice resultat[][SIZE], matrice matrice1[][SIZE], matrice matrice2[][SIZE]){     //taille fixée
     int stockage = 0;
     for (int i=0;i<SIZE;i++){
         for (int j=0;j<SIZE;j++){
@@ -12,6 +13,26 @@ void matrix_mult(matrice resultat[][SIZE], matrice matrice1[][SIZE], matrice mat
                 stockage += matrice1[k][i] * matrice2[j][k];
             }
             resultat[i][j]=stockage;
+            stockage=0;
+        }
+    }
+}
+
+typedef struct{
+    int ligne;
+    int colonne;
+    int **valeur;
+}Matrix;
+
+
+void matrix_mult_2(Matrix resultat, Matrix matrice1, Matrix matrice2){      //taille quelconque
+    int stockage = 0;
+    for (int i=0;i<SIZE;i++){
+        for (int j=0;j<SIZE;j++){
+            for (int k=0;k<SIZE;k++){
+                stockage += matrice1.valeur[k][i] * matrice2.valeur[j][k];
+            }
+            resultat.valeur[i][j]=stockage;
             stockage=0;
         }
     }
